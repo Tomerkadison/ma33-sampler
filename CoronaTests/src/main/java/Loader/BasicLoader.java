9d66f88113ea879018ff4c7586e1eec9a2023c09
Tomer <kadison.tomer@gmail.commy>
2022-01-17 08:45:25 +0200

    fixing the file seperation

--- CoronaTests/src/main/java/Loader/BasicLoader.java
+++ CoronaTests/src/main/java/Loader/BasicLoader.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class BasicLoader implements Loader {
     private String path;
@@ -17,19 +20,20 @@ public class BasicLoader implements Loader {
     private DataWriter dataWriter;
 
     public BasicLoader(String path, String type) {
-        this.path = path;
         this.type = type;
-        this.dataWriter = new DataWriterFactories(path).get(type).create();
+        this.path = path +"."+ type;
+        this.dataWriter = new DataWriterFactories(this.path).get(this.type).create();
     }
 
     @Override
     public void load() {
         ObjectMapper mapper = new ObjectMapper();
-        for (int i = 0; i < this.data.getData().size() / 50000; i++) {
-            dataWriter.writeRecords(this.data.getRecords(i*50000,(i+1)*50000));
+        for (int i = 0; i <(this.data.getData().size()-1)/ 500; i++) {
+            dataWriter.writeRecords(this.data.getRecords(i*100,(i+1)*100));
             this.path = this.path.substring(0,this.path.length()-this.type.length()-1) + "l."+type;
+            this.dataWriter = new DataWriterFactories(this.path).get(this.type).create();
         }
-        this.dataWriter.writeRecords(this.data.getRecords(5000*(this.data.getData().size() / 50000),this.data.getData().size() -1));
+        this.dataWriter.writeRecords(this.data.getRecords(100*((this.data.getData().size()-1) / 100),this.data.getData().size()));
     }
     public void setData(DataManager data){
         this.data = data;
--- CoronaTests/src/main/java/Loader/Writer/JsonWriter.java
+++ CoronaTests/src/main/java/Loader/Writer/JsonWriter.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class JsonWriter implements DataWriter{
+public class JsonWriter implements DataWriter {
     private String path;
 
     public JsonWriter(String path) {
@@ -22,8 +22,8 @@ public class JsonWriter implements DataWriter{
     public void writeRecords(List<HashMap<String, String>> records) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            mapper.writeValue(Paths.get(this.path).toFile(),records);
-        }catch (JsonMappingException e) {
+            mapper.writeValue(Paths.get(this.path).toFile(), records);
+        } catch (JsonMappingException e) {
             e.printStackTrace();
         } catch (JsonGenerationException e) {
             e.printStackTrace();
