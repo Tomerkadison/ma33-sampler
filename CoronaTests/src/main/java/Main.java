8b360713fbd1f976f7b92b9326d153718b184c08
Tomer <kadison.tomer@gmail.commy>
2022-01-17 09:32:53 +0200

    changing directory name to mada_reports

--- CoronaTests/src/main/java/Main.java
+++ CoronaTests/src/main/java/Main.java
@@ -5,7 +5,7 @@ import Loader.FileLoader;
 public class Main {
     public static void main(String[] args) {
         CsvExtractor csvExtractor = new CsvExtractor("src/main/resources/MadaReports.csv");
-        FileLoader basicLoader = new FileLoader("C:\\Users\\kadis\\Desktop\\temp\\mada_reports", "json");
+        FileLoader basicLoader = new FileLoader("C:\\Users\\kadis\\Desktop\\mada_reports\\mada_reports", "json");
         BasicETL etl = new BasicETL(csvExtractor, basicLoader);
         etl.execute();
     }
