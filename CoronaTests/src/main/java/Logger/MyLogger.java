7c944ab718739b437fa903ed7ddaadb5f46d52c5
Tomer <kadison.tomer@gmail.commy>
2022-01-18 15:55:28 +0200

    solving level 3 for now

--- CoronaTests/src/main/java/Logger/MyLogger.java
+++ CoronaTests/src/main/java/Logger/MyLogger.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class MyLogger {
     private final Logger logger =
             Logger.getLogger("com.javacodegeeks.snippets.core");
 
     public Logger getLogger() {
-        return  logger;
-
+        return logger;
     }
 }
